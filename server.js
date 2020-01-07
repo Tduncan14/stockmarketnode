aff85e316b95b7b42c5e82d8244d00b613dc5cc4
Treek <nbatre1@yahoo.com>
2020-01-06 21:22:20 -0600

    keep moving forward

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const app = express();
+
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 
-const app = express()
+
+// registers hbs.engine with the express app and set handlebars middleware
+app.engine('handlebars',exphbs());
+app.set('view engine','handlebars');
 
 
 
+app.get('/', function(req,res){
+
+    res.render('home');
+})
 
 const PORT  = 8080 || process.env.PORT;
+
+
+// express.static with path.join to point to a file to display;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.listen(PORT,( req,res,next) => (
